385e9db50b4d7ef9db00f9fc6a42c50862bd435d
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2020-05-10 17:09:27 +0700

    first commit

---
+++ resources/lang/en/message.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'home' => 'HOME',
+    'about' => 'ABOUT US',
+    'album' => 'ALBUM',
+    'contact' => 'CONTACT US',
+    'language' => 'Language',
+    'th' => 'THAI',
+    'en' => 'ENGLISH'
+];
